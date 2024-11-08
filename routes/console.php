b2b0835c3c6c7136019edcd8a5d866dc001cfc72
Solly <solobarine@gmail.com>
2024-11-08 21:50:25 +0100

    scheduler: schedule hourly batch user updates

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Jobs\HandleUserUpdate;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// Get Updates from Cache
+$updates = Cache::has('user_updates') && Cache::get('user_updates')->count() > 0 ? Cache::pull('user_updates') : [];
+
+// Schedule Job
+Schedule::job(new HandleUserUpdate($updates))->hourly()->when(function ($updates) {
+    count($updates) > 0;
+});
