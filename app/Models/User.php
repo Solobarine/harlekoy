f502031696b447203c19312d4b2129471e356513
Solly <solobarine@gmail.com>
2024-11-08 21:49:28 +0100

    user: queue user on field update

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,26 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Jobs\HandleUserUpdate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Cache;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::updated(function ($user) {
+            self::queueUserUpdate($user);
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -46,5 +58,25 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected static function queueUserUpdate(User $user)
+    {
+        $updatedFields = $user->getDirty();
+
+        $data = ['email' => $user->email];
+
+        foreach ($updatedFields as $key => $value) {
+            $data[$key] = $value;
+        }
+
+        // Store in cache until the batch has 1000 updates
+        Cache::add('user_updates', $data);
+
+        // Dispatch job if batch has 1000 updates
+        if (Cache::get('user_updates')->count() >= 1000) {
+            $updates = Cache::pull('user_updates');
+            HandleUserUpdate::dispatch($updates);
+        }
     }
 }
